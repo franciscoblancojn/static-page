478fd2007d060ff861f2eb3a6d09590a737f1bb3
Francisco Blanco <blancofrancisco34@gmail.com>
2026-05-11 23:11:11 -0500

    remove get-html

--- src/api/_.php
+++ src/api/_.php
@@ -1,5 +1,4 @@
 <?php
 require_once STPA_DIR . 'src/api/api.php';
-require_once STPA_DIR . 'src/api/get-html.php';
 require_once STPA_DIR . 'src/api/set-html.php';
 require_once STPA_DIR . 'src/api/set-post-config.php';
